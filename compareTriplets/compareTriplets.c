991967bf32ea94ea578e2597973cb68b4a201faf
Yazan Ghunaim <87646431+YazanGhunaim@users.noreply.github.com>
2022-11-23 12:31:28 +0100

    Update compareTriplets.c

--- compareTriplets/compareTriplets.c
+++ compareTriplets/compareTriplets.c
@@ -16,42 +16,6 @@ char** split_string(char*);
 
 int parse_int(char*);
 
-/*
- * Complete the 'compareTriplets' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY a
- *  2. INTEGER_ARRAY b
- */
-
-/*
- * To return the integer array from the function, you should:
- *     - Store the size of the array to be returned in the result_count variable
- *     - Allocate the array statically or dynamically
- *
- * For example,
- * int* return_integer_array_using_static_allocation(int* result_count) {
- *     *result_count = 5;
- *
- *     static int a[5] = {1, 2, 3, 4, 5};
- *
- *     return a;
- * }
- *
- * int* return_integer_array_using_dynamic_allocation(int* result_count) {
- *     *result_count = 5;
- *
- *     int *a = malloc(5 * sizeof(int));
- *
- *     for (int i = 0; i < 5; i++) {
- *         *(a + i) = i + 1;
- *     }
- *
- *     return a;
- * }
- *
- */
 int* compareTriplets(int a_count, int* a, int b_count, int* b, int* result_count) {
     
     *result_count = 2;
