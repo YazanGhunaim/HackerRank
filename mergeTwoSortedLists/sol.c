8982b94a12bf5cb17dc6a796cacacf63e9cee15b
Yazan Ghunaim <yazanghunaim07@gmail.com>
2022-12-14 17:42:31 +0100

    merge two sorted linked lists solution

---
+++ mergeTwoSortedLists/sol.c
@@ -0,0 +1,113 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+typedef struct SinglyLinkedListNode SinglyLinkedListNode;
+typedef struct SinglyLinkedList SinglyLinkedList;
+
+struct SinglyLinkedListNode {
+    int data;
+    SinglyLinkedListNode* next;
+};
+
+struct SinglyLinkedList {
+    SinglyLinkedListNode* head;
+    SinglyLinkedListNode* tail;
+};
+
+SinglyLinkedListNode* create_singly_linked_list_node(int node_data) {
+    SinglyLinkedListNode* node = malloc(sizeof(SinglyLinkedListNode));
+
+    node->data = node_data;
+    node->next = NULL;
+
+    return node;
+}
+
+void insert_node_into_singly_linked_list(SinglyLinkedList** singly_linked_list, int node_data) {
+    SinglyLinkedListNode* node = create_singly_linked_list_node(node_data);
+
+    if (!(*singly_linked_list)->head) {
+        (*singly_linked_list)->head = node;
+    } else {
+        (*singly_linked_list)->tail->next = node;
+    }
+
+    (*singly_linked_list)->tail = node;
+}
+
+void print_singly_linked_list(SinglyLinkedListNode* node, char* sep, FILE* fptr) {
+    while (node) {
+        fprintf(fptr, "%d", node->data);
+
+        node = node->next;
+
+        if (node) {
+            fprintf(fptr, "%s", sep);
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the mergeLists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    SinglyLinkedListNode * head = (SinglyLinkedListNode *)malloc(sizeof(*head));
+    SinglyLinkedListNode * temp = head;
+    
+    
+    if(!head1){
+        temp = head2;
+        return temp;
+    }
+    
+    if(!head2){
+        temp = head1;
+        return temp;
+    }
+    
+    while(head1 && head2){
+        if(head1 -> data < head2 -> data){
+            temp -> next = head1;
+            head1 = head1 -> next;
+        }
+        else{
+            temp -> next = head2;
+            head2 = head2 -> next;
+        }
+    temp = temp -> next;
+    }
+    
+    if(head1) temp -> next = head1;
+    if(head2) temp -> next = head2;
+    
+    return head -> next;
+}
+
+
+int main()
